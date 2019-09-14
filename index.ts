bc9996f141fbd81eba9fed96b538a4b73ec5a234
srinivasdareddy <srinivasdareddy@gmail.com>
2019-09-14 17:03:19 +0530

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
 // Import stylesheets
 import './style.css';
 
-/**
- * null in javascript : null refers to an empty of non-existed value
- * */
- 
-var name = null;
-
-console.log('value of name:', name); // prints 'null'
-
-var name1;
-
-console.log(name1); // prints 'undefined'
+console.log('index')
+import * as other from './other';
+other.run()
---
+++ other.ts
@@ -0,0 +1,13 @@
+export function run(){
+/**
+ * null in javascript : null refers to an empty of non-existed value
+ * */
+ 
+var name = null;
+
+console.log('value of name:', name); // prints 'null'
+
+var name1;
+
+console.log(name1); // prints 'undefined'
+}
